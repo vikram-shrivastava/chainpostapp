e89512984a8d2ff5caf96965ae0b7903854b5b28
Vikram Shrivastav <vikrams13122004@gmail.com>
2025-11-18 15:09:25 +0530

    update User model

--- src/app/api/onboarding/route.js
+++ src/app/api/onboarding/route.js
@@ -1,7 +1,7 @@
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import dbConnect from '@/db';
-import User from "../../../models/User.model.js";
+import User from "@/models/User.model";
 export const POST = async (request) => {
 try {
     await dbConnect();
